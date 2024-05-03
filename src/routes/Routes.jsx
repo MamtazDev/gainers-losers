abceea38359b7c643a612c63aec90da1112a58d9
MamtazDev <akter.mstmamtaz@gmail.com>
2024-05-03 21:55:34 +0600

    country list sioign

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout.jsx";
 import Home from "../pages/Home.jsx";
+import Signin from "../pages/Signin.jsx";
+import CountryList from "../pages/CountryList.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/country-list",
+        element: <CountryList />,
       },
     ],
   },
